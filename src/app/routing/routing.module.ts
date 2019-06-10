b03f946ae9509ad9992453da29bd9d2439c92b92
vincent <ododovincent54@gmail.com>
2019-06-10 10:10:17 +0300

    updated my Routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+
+import { GitReposComponent } from '../git-repos/git-repos.component';
+import { GithubUsersComponent } from '../github-users/github-users.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes:Routes=[
+  {path:"home",component:GithubUsersComponent},
+  {path:"repos",component:GitReposComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
 })
 export class RoutingModule { }
